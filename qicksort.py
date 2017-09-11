282b4bb8b9365e1241ac1cf884fd2737f7df0af6
ArtemioG <30916160+ArtemioG@users.noreply.github.com>
2017-09-11 11:40:09 -0500

    Add files via upload

--- qicksort.py
+++ qicksort.py
@@ -13,18 +13,8 @@ def quicksort(arr):
             menores.append(e)
         else:
             mayores.append(e)
-    return quicksort(menores)+[p]+qicksort(mayores)
-def rndar(long):
-    arr=[]
-    for i in range(long):
-        arr.append(random,randint(0,long))
-    return arr
-l=10
+    return quicksort(menores)+[p]+quicksort(mayores)
 
-while l<=10:
-    for replica in range(10):
-        ori=rndar(1)
-        arr=qicksort(ori)
-        print(1,cnt,arr,ori)
-        cnt=0
-    l*=2
+a=[123,45,1,2300,-12,1,-6]
+print(a)
+quicksort(a)
